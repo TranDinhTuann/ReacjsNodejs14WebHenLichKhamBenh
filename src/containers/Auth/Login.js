16663f434bbf6855b1238b6a610f37982f84d13f
TranDinhTuann <dtuan12122002@gmail.com>
2024-02-28 13:59:02 +0700

    phut 41 bai 36

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -6,33 +6,67 @@ import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
 import "./Login.scss";
 import { FormattedMessage } from "react-intl";
+import {handleLoginApi} from "../../services/userServices";
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
-      password: "",
+      username: '',
+      password: '',
+      isShowPassword: false,
+      errMessage: ''
     };
   }
 
   handleOnChangeInput = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     this.setState({
         username: event.target.value
+
     })
   }
   
   handleOnChangePassword = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     this.setState({
         password: event.target.value
     })
   }
 
-  handleLogin = () => {
-    console.log(`Username: ${this.state.username}, Username: ${this.state.password}`);
-    console.log('Allstate:' + this.state);
+  handleLogin = async () => {
+    this.setState({
+      errMessage: ''
+    })
+    // console.log(`Username: ${this.state.username}, Password: ${this.state.password}`);
+    // console.log('all state:', this.state);
+    try {
+      let  data = await handleLoginApi(this.state.username, this.state.password);
+      if (data && data.errCode !== 0){
+        this.setState({
+          errMessage: data.message
+        })
+      }
+      if(data && data.errCode === 0 ){
+        // todo
+        console.log('login success');
+      }
+    } catch (error) {
+      if(error.response){
+        if(error.response.data){
+          this.setState({
+            errMessage: error.response.data.message
+          })
+        }
+      }
+      console.log('hoidanit', error.response)
+    }
+  }
+
+  handleShowHidePassword = () => {
+    this.setState({
+      isShowPassword: !this.state.isShowPassword,
+    })
   }
 
   render() {
@@ -52,22 +86,39 @@ class Login extends Component {
                 onChange={(event) => this.handleOnChangeInput(event)}
               ></input>
             </div>
-            <div className="col-12 form-group">
+            <div className="col-12 form-group login-input">
               <label for="input-password">Password:</label>
-              <input
-                type="text"
-                className="form-control"
-                id="input-password"
-                placeholder="Enter your password"
-                value={this.state.password}
-                onChange={(event) => this.handleOnChangePassword(event)}
-              ></input>
+              <div className="custom-input-password">
+                <input
+                  type={this.state.isShowPassword ? 'text' : 'password'}
+                  className="form-control"
+                  id="input-password"
+                  placeholder="Enter your password"
+                  onChange={(event) => this.handleOnChangePassword(event)}
+                ></input>
+                <span
+                  onClick={() => {this.handleShowHidePassword()}}
+                  ><i className={this.state.isShowPassword ? "far fa-eye" : "far fa-eye-slash"}></i>
+                </span>
+              </div>
             </div>
-            <div className="col-12 form-group">
-              <button className="button-login" onClick={()=>this.handleLogin()}>Login</button>
+
+            <div className="col-12" style={{color: 'red'}}>
+              {this.state.errMessage}
             </div>
+
             <div className="col-12 form-group">
-              <span>Forgot your password?</span>
+              <button className="btn-login" onClick={() => {this.handleLogin()} }>Login</button>
+            </div>
+            <div className="col-12">
+              <span className="forgot-password">Forgot your password?</span>
+            </div>
+            <div className="col-12 text-center">
+              <span className="text-other-login">Or login with:</span>
+            </div>
+            <div className="col-12 social-login">
+              <i className="fab fa-google-plus-g google"></i>
+              <i className="fab fa-facebook-f facebook"></i>
             </div>
           </div>
         </div>
